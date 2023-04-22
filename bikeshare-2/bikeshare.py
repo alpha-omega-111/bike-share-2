94817c2471fe7f86824cb2cbb6a55982ebc0ced2
Igor Racic <igor.racic@angloamerican.com>
2023-04-22 15:10:13 +0100

    Added additional docstring in the get_filders() on Args

--- bikeshare-2/bikeshare.py
+++ bikeshare-2/bikeshare.py
@@ -11,6 +11,11 @@ def get_filters():
 
     Input required: Asks user to specify a city, month, and day to analyze.
 
+    Args:
+        (str) city - name of the city to analyze, not sensitive to capitalisation
+        (str) month - name of the month to filter by, or "all" to apply no month filter, not sensitive to capitalisation
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter, not sensitive to capitalisation
+
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
